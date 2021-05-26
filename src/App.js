1733756bcbff39f8c79fd62e0b52499a04c5c0d6
Aura Cristina <auracristina0693@gmail.com>
2021-05-25 19:07:58 -0500

    solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
-      </div>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <span className="value">0</span>
+//         <button id="inc">Incrementa</button>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <span className="value">{value}</span>
+      <button id="inc" onClick={() => setValue(value + 1)}>
+        Incrementa
+      </button>
+    </div>
+  );
 }
 
 export default App;
